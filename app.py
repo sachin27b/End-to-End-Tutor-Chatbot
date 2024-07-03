52dc655d06f140fa61ae90084c908a84efce1166
sachin <sachin270895@gmail.com>
2024-07-03 15:23:22 +0530

    done

--- app.py
+++ app.py
@@ -31,22 +31,37 @@ qa=RetrievalQA.from_chain_type(
     return_source_documents=True, 
     chain_type_kwargs=chain_type_kwargs)
 
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
+
+
+
+# @app.route("/get", methods=["GET", "POST"])
+# def chat():
+#     msg = request.form["msg"]
+#     input = msg
+#     print(input)
+#     result=qa({"query": input})
+#     print("Response : ", result["result"])
+#     return str(result["result"])
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-
-
 @app.route("/get", methods=["GET", "POST"])
 def chat():
-    msg = request.form["msg"]
+    if request.method == "POST":
+        msg = request.form["msg"]
+    else:
+        msg = request.args.get("msg")
+    
     input = msg
     print(input)
-    result=qa({"query": input})
-    print("Response : ", result["result"])
+    result = qa({"query": input})
+    print("Response: ", result["result"])
     return str(result["result"])
-
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port= 8080, debug= True)
